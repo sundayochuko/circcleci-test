9f19c011736f0c2b87f4f017f89a86767626f50b
Sunday Ochuko <sundayochuko101@gmail.com>
2024-02-17 05:14:24 +0200

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 http
   .createServer(function (req, res) {
-    res.write("Hello World Server!");
+    res.write("Hello World!");
     res.end();
   })
   .listen(8080, "0.0.0.0");
